45f8a7660383fa8167f744e9a4beb1a817756c2d
CosynPa <v_ds_dt@163.com>
2016-05-25 13:53:40 +0800

    Initial Commit

---
+++ ViewModelProperty/ViewModelProperty.h
@@ -0,0 +1,19 @@
+//
+//  ViewModelProperty.h
+//  ViewModelProperty
+//
+//  Created by Pan Yusheng on 5/24/16.
+//  Copyright © 2016 FansUp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewModelProperty.
+FOUNDATION_EXPORT double ViewModelPropertyVersionNumber;
+
+//! Project version string for ViewModelProperty.
+FOUNDATION_EXPORT const unsigned char ViewModelPropertyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModelProperty/PublicHeader.h>
+
+
