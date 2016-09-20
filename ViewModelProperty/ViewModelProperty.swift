39cd3db52b820ff22d21628ccb7d09bd406e1593
Pan Yusheng <v_ds_dt@163.com>
2016-09-20 17:17:04 +0800

    Add discardable attribute, add comments, remove old method

--- ViewModelProperty/ViewModelProperty.swift
+++ ViewModelProperty/ViewModelProperty.swift
@@ -29,6 +29,7 @@ public final class ViewModelProperty<Value, UpdateInfo, ActionInfo> {
     
     // MARK: - Derived signals
     
+    /// Like updateSignal but send current value immediately
     public private(set) lazy var updateProducer: SignalProducer<(Value, CurrentOrUpdate<UpdateInfo>), NoError> = { [unowned self] in
         return SignalProducer { observer, producerDisposable in
             observer.sendNext((self._value, .current))
@@ -39,6 +40,7 @@ public final class ViewModelProperty<Value, UpdateInfo, ActionInfo> {
         }
     }()
     
+    /// Send current value immediately and send both updates and actions
     public private(set) lazy var allChangeProducer: SignalProducer<(Value, CurrentOrUpdateOrAction<UpdateInfo, ActionInfo>), NoError> = { [unowned self] in
         return SignalProducer { observer, producerDisposable in
             observer.sendNext((self._value, .current))
@@ -60,30 +62,6 @@ public final class ViewModelProperty<Value, UpdateInfo, ActionInfo> {
     
     // MARK: -
     
-    public private(set) lazy var changingProducer: SignalProducer<Value, NoError> = { [unowned self] () -> SignalProducer<Value, NoError> in
-        return SignalProducer<Value, NoError> { observer, producerDisposable in
-            self.updateProducer.startWithSignal { signal, cancelDisposable in
-                producerDisposable += cancelDisposable
-
-                signal.observe { event in
-                    switch event {
-                    case .next(let valueAndInfo):
-                        observer.sendNext(valueAndInfo.0)
-                    case .failed, .interrupted: // no possible
-                        break
-                    case .completed:
-                        observer.sendCompleted()
-                    }
-                    
-                }
-            }
-            
-            producerDisposable += self.actionSignal.observeNext { valueAndInfo in
-                observer.sendNext(valueAndInfo.0)
-            }
-        }
-    }()
-    
     private let lock: NSRecursiveLock
     private var _value: Value
     
@@ -104,6 +82,7 @@ public final class ViewModelProperty<Value, UpdateInfo, ActionInfo> {
     }
     
     /// Set the value by program update such as network callback. Returns the old value.
+    @discardableResult
     public func setValueByUpdate(_ newValue: Value, info: UpdateInfo) -> Value {
         lock.lock()
         defer { lock.unlock() }
@@ -115,6 +94,7 @@ public final class ViewModelProperty<Value, UpdateInfo, ActionInfo> {
     }
     
     /// Set the value by user action, e.g. user edit a text field. Returns the old value.
+    @discardableResult
     public func setValueByAction(_ newValue: Value, info: ActionInfo) -> Value {
         lock.lock()
         defer { lock.unlock() }
@@ -167,18 +147,22 @@ infix operator +> : DefaultPrecedence
 
 // These two operators make the direction of data flow clearer.
 
+@discardableResult
 public func <+ <Value, UpdateInfo, ActionInfo>(property: ViewModelProperty<Value, UpdateInfo, ActionInfo>, valueAndInfo: (Value, UpdateInfo)) -> Value {
     return property.setValueByUpdate(valueAndInfo.0, info: valueAndInfo.1)
 }
 
+@discardableResult
 public func +> <Value, UpdateInfo, ActionInfo>(valueAndInfo: (Value, ActionInfo), property: ViewModelProperty<Value, UpdateInfo, ActionInfo>) -> Value {
     return property.setValueByAction(valueAndInfo.0, info: valueAndInfo.1)
 }
 
+@discardableResult
 public func <+ <Value, UpdateInfo: NoInfoType, ActionInfo>(property: ViewModelProperty<Value, UpdateInfo, ActionInfo>, value: Value) -> Value {
     return property.setValueByUpdate(value, info: UpdateInfo())
 }
 
+@discardableResult
 public func +> <Value, UpdateInfo, ActionInfo: NoInfoType>(value: Value, property: ViewModelProperty<Value, UpdateInfo, ActionInfo>) -> Value {
     return property.setValueByAction(value, info: ActionInfo())
 }
